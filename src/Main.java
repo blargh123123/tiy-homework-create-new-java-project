c1e21011dce11251f7048bf4c01a3454049ca68e
Doug Hughes <dhughes@doughughes.net>
2017-02-17 08:31:43 -0500

    completed assignment

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+/**
+ * Created by doug on 2/17/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(1+1);
+
+        System.out.println(24/2);
+
+        System.out.println("foo " + 123);
+
+        System.out.println(123 % 3);
+
+        System.out.println(5 + 3 - 2);
+
+
+        System.out.println(123 * 3);
+
+
+        System.out.println(123 / 3);
+
+        System.out.println(123 % 7 * (5 / 2));
+
+        System.out.println(8 - 3);
+
+
+        System.out.println("sup?");
+    }
+}
